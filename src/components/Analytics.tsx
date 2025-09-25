f36942ebbb49632f3a08c45d761c16ff17a36f4b
aukri1690 <toro369@icloud.com>
2025-09-25 12:38:07 +0900

    style: change layout

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -41,7 +41,7 @@ const Analytics = () => {
   const [pullRequestCount, setPullRequestCount] = useState<number | null>(null);
   const [pullRequestOverTime, setPullRequestOverTime] = useState<any>(null);
   const [pullRequestHourlyDistribution, setPullRequestHourlyDistribution] = useState<any>(null);
-  const [ changeLinesScatter, setChangeLinesScatter ] = useState<any>(null);
+  const [changeLinesScatter, setChangeLinesScatter] = useState<any>(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -107,86 +107,83 @@ const Analytics = () => {
   ) : (
     <>
       <Flex justify='center' align='center' minH='100vh'>
-        <Card.Root textAlign='center' variant='outline' width='1080px' height='540px' p={5}>
-          <Card.Title fontSize='3xl' fontWeight={900} mt={3} mb={3}>Pull Requests Over Time</Card.Title>
-          <Box h='100%'>
-            <Line
-              data={pullRequestOverTime}
-              options={{
-                responsive: true,
-                maintainAspectRatio: false,
-                plugins: { legend: { display: false } },
-                scales: {
-                  y: {
-                    ticks: { precision: 0 }
-                  }
-                },
-              }}
-            />
-          </Box>
-        </Card.Root>
-      </Flex>
-
-      <Flex justify='center' align='center' minH='100vh'>
-        <Card.Root textAlign='center' variant='outline' width='1080px' height='540px' p={5}>
-          <Card.Title fontSize='3xl' fontWeight={900} mt={3} mb={3}>Pull Requests Hourly Distribution</Card.Title>
-          <Box h='100%'>
-            <Bar
-              data={pullRequestHourlyDistribution}
-              options={{
-                responsive: true,
-                maintainAspectRatio: false,
-                plugins: { legend: { display: false } },
-                scales: {
-                  y: {
-                    beginAtZero: true,
-                    ticks: { precision: 0 }
-                  }
-                },
-              }}
-            />
-          </Box>
-        </Card.Root>
-      </Flex>
-
-      <Flex justify='center' align='center' minH='100vh'>
-        <Card.Root textAlign='center' variant='outline' width='1080px' height='540px' p={5}>
-          <Card.Title fontSize='3xl' fontWeight={900} mt={3} mb={3}>Percentage of changed lines</Card.Title>
-          <Box h='100%'>
-            <Scatter
-              data={changeLinesScatter}
-              options={{
-                responsive: true,
-                maintainAspectRatio: false,
-                plugins: { legend: { display: false }, tooltip: { mode: 'nearest', intersect: false } },
-                scales: {
-                  x: {
-                    type: 'linear',
-                    title: { display: true, text: 'Additions' },
-                    beginAtZero: true,
-                    min: 0,
-                    max: 100,
-                  },
-                  y: {
-                    title: { display: true, text: 'Deletions' },
-                    beginAtZero: true,
-                    min: 0,
-                    max: 50,
-                  }
-                }
-              }}
-            />
-          </Box>
-        </Card.Root>
-      </Flex>
-
-      <Flex justify='center' align='center' minH='100vh'>
-        <Card.Root textAlign='center' variant='outline' width='250px' height='250px'>
-          <Card.Title fontSize='2xl' fontWeight={900} mt={8} mb={-8}>Total Pull Requests</Card.Title>
-          <Flex flex='1' justify='center' align='center'>
-            <Card.Title fontSize='8xl' fontFamily='Impact' color='orange'>{pullRequestCount}</Card.Title>
+        <Flex direction='column'>
+          <Flex direction='row'>
+            <Card.Root textAlign='center' variant='outline' width='500px' height='350px'>
+              <Card.Title fontSize='2xl' fontWeight={900} mt={8} mb={-8}>Total Pull Requests</Card.Title>
+              <Flex flex='1' justify='center' align='center'>
+                <Card.Title fontSize='8xl' fontFamily='Impact' color='orange'>{pullRequestCount}</Card.Title>
+              </Flex>
+            </Card.Root>
+            <Card.Root textAlign='center' variant='outline' width='900px' height='350px' p={5}>
+              <Card.Title fontSize='3xl' fontWeight={900} mt={3} mb={3}>Pull Requests Over Time</Card.Title>
+              <Box h='100%'>
+                <Line
+                  data={pullRequestOverTime}
+                  options={{
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    plugins: { legend: { display: false } },
+                    scales: {
+                      y: {
+                        ticks: { precision: 0 }
+                      }
+                    },
+                  }}
+                />
+              </Box>
+            </Card.Root>
           </Flex>
-        </Card.Root>
+          <Flex direction='row'>
+            <Card.Root textAlign='center' variant='outline' width='500px' height='350px' p={5}>
+              <Card.Title fontSize='3xl' fontWeight={900} mt={3} mb={3}>Percentage of changed lines</Card.Title>
+              <Box h='100%'>
+                <Scatter
+                  data={changeLinesScatter}
+                  options={{
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    plugins: { legend: { display: false }, tooltip: { mode: 'nearest', intersect: false } },
+                    scales: {
+                      x: {
+                        type: 'linear',
+                        title: { display: true, text: 'Additions' },
+                        beginAtZero: true,
+                        min: 0,
+                        max: 100,
+                      },
+                      y: {
+                        title: { display: true, text: 'Deletions' },
+                        beginAtZero: true,
+                        min: 0,
+                        max: 50,
+                      }
+                    }
+                  }}
+                />
+              </Box>
+            </Card.Root>
+            <Card.Root textAlign='center' variant='outline' width='900px' height='350px' p={5}>
+              <Card.Title fontSize='3xl' fontWeight={900} mt={3} mb={3}>Pull Requests Hourly Distribution</Card.Title>
+              <Box h='100%'>
+                <Bar
+                  data={pullRequestHourlyDistribution}
+                  options={{
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    plugins: { legend: { display: false } },
+                    scales: {
+                      y: {
+                        beginAtZero: true,
+                        ticks: { precision: 0 }
+                      }
+                    },
+                  }}
+                />
+              </Box>
+            </Card.Root>
+          </Flex>
+        </Flex>
       </Flex>
     </>
   );
