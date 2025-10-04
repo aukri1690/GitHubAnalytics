23ed776062f80438361441e6e5b48edf11ded0bb
aukri1690 <toro369@icloud.com>
2025-10-04 15:55:57 +0900

    refactor: improvement error handling

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,14 +2,13 @@ import { NextResponse } from 'next/server';
 import { graphql } from '@octokit/graphql';
 
 const GET = async () => {
-  try {
-    const client = graphql.defaults({
-      headers: {
-        authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-      },
-    });
+  const octokit = graphql.defaults({
+    headers: {
+      authorization: `token ${process.env.GITHUB_TOKEN}`,
+    },
+  });
 
-    const query = `
+  const query = `
       query($cursor: String) {
         viewer {
           pullRequests(
@@ -29,12 +28,13 @@ const GET = async () => {
       }
     `;
 
-    const data = await client(query, { cursor: null});
-
+  try {
+    const data = await octokit(query, { cursor: null });
+    console.log('データを取得できました');
     return NextResponse.json(data);
   } catch (error) {
-    console.error(error);
-    return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500 });
+    console.error('データの取得に失敗しました', error);
+    return NextResponse.json({ error: 'データの取得に失敗しました' }, { status: 500 });
   }
 };
 
